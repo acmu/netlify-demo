07a8c9e7f1daceac27c2bd32693e40b9b17a2f6e
CodeCat <s1124yy@qq.com>
2018-09-25 17:20:26 +0800

    v

--- output/js/demo.js
+++ output/js/demo.js
@@ -2,13 +2,13 @@ let cc = (...val) => console.log(...val);
 
 window.onload = function() {
   let uli = document.querySelectorAll('ul>li');
+  let headImg = document.querySelector('.head>.img');
   for(let i=0;i<uli.length;i++) {
     setTimeout(() => {
       uli[i].style.animation = 'flip 1s linear';
       uli[i].style.opacity = '1';
     }, 300*i);
   }
-  let headImg = document.querySelector('.head>.img');
   headImg.style.opacity = '1';
 }
 
