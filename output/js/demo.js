5bb26df28524f48638c7bcac7571f860030fe63f
CodeCat <s1124yy@qq.com>
2018-09-25 23:33:22 +0800

    v

--- output/js/demo.js
+++ output/js/demo.js
@@ -2,13 +2,16 @@ let cc = (...val) => console.log(...val);
 
 window.onload = function() {
   let uli = document.querySelectorAll('ul>li');
-  let headImg = document.querySelector('.head>.img');
+  let img = document.querySelectorAll('img');
   for(let i=0;i<uli.length;i++) {
     setTimeout(() => {
       uli[i].style.animation = 'flip 1s linear';
       uli[i].style.opacity = '1';
     }, 300*i);
   }
-  headImg.style.opacity = '1';
+  img.onload = function() {
+    let headImg = document.querySelector('.head>.img');
+    headImg.style.opacity = '1';
+  }
 }
 
